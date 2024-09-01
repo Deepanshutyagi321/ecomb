6ed4f23a9e1c8eab692343babe365a2f304c8e6f
deepanshutyagi321 <deepanshuTyagi97208@gamil.com>
2024-09-01 12:53:11 +0530

    edit url to cors

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const DB_url = process.env.MONGODB_URL;
 
 
 app.use(cors({
-    origin: 'https://ecomf-one.vercel.app/',
+    origin: ['http://localhost:5173', 'https://your-production-domain.com'],
     credentials: true
 }));
 app.use(express.urlencoded({ extended: true }));
